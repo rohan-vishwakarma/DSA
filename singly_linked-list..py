0b897cdd89c9253f157911df18d53177db75fd1f
Rohan Vishwakarma <116492067+rohan-vishwakarma@users.noreply.github.com>
2023-12-27 18:26:55 +0530

    Create singly_linked-list..py

---
+++ singly_linked-list..py
@@ -0,0 +1,66 @@
+class Node:
+
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+# 1 --> 2 -- > 3 -->4
+class LinkedList:
+
+	def __init__(self):
+		self.head = None
+		self.next = None
+		self.length = 0
+
+	def addElement(self, value):
+		newNode = Node(value)
+		if self.head is None:
+			self.head = newNode
+			self.tail = newNode
+			self.head.next = None
+		else:
+			self.tail.next = newNode
+			newNode.next = None
+			self.tail = newNode
+		self.length +=1
+
+	def prepend(self, value):
+		new_node = Node(value)
+		if self.head is not None:
+			temp_node = self.head
+			self.head = new_node
+			new_node.next = temp_node
+		self.length +=1
+
+	def addAtPosition(self, index, value ):
+
+		if index == 0:
+			self.prepend(value)
+		else:
+
+			new_node = Node(value)
+			temp_node = self.head
+			for _ in range(index -1):
+				temp_node = temp_node.next
+			t = temp_node.next
+			temp_node.next = new_node
+			new_node.next = t
+		self.length +=1
+
+	def __str__(self):
+		temp_node = self.head
+		res = ''
+		while temp_node is not None:
+			res += str(temp_node.data)
+			temp_node =temp_node.next
+			if temp_node is not None:
+				res += '-->'
+		return res
+
+obj = LinkedList()
+element = obj.addElement(12)
+element = obj.addElement(13)
+element = obj.addElement(14)
+element = obj.addElement(15)
+element = obj.addAtPosition(3, 100)
+print(obj)
