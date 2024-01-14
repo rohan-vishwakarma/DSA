2438330f68d892a55d78dae99d15eb1584cbea8e
rohan-vishwakarma <rohanvishwakarma685@gmail.com>
2024-01-14 16:45:41 +0530

    sunday 4 pm at home

---
+++ dictionary.py
@@ -0,0 +1,20 @@
+
+
+dict = {
+
+    "name" : "rohan",
+    "Age" : " 19 Yo",
+    "place" : "Thane"
+}
+
+
+dict["name"] = "Kayva"
+
+dict["pincode"] = 400605
+
+dict["Address"] = list
+
+dict['Address'][list] 
+
+
+print(dict)
