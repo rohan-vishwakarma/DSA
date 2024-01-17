b05f69f219b5bb189a15e5824da8ff74739c8092
rohan-vishwakarma <rohanvishwakarma685@gmail.com>
2024-01-17 22:20:57 +0530

    commited Enumeration at 10: 20

---
+++ library/enumeration_program.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class Color(Enum):
+    Red = 1
+    Blue = 2
+    Green = 3
+    Yellow = 4
+
+
+for elements in Color:
+    print(elements.name, " = > ", elements.value)
