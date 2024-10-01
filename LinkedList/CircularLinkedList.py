8a8333ddddec7a6767430fcf98b90899abef2887
rohan vishwakarma win10 <rohanvishwakarma685@gmail.com>
2024-10-01 17:29:31 +0530

    dd

---
+++ LinkedList/CircularLinkedList.py
@@ -0,0 +1,49 @@
+#                   +------+         +------+         +------+
+#                   |  A   | ------> |  B   | ------> |  C   |
+#                   +------+         +------+         +------+
+#                      ^                                   |
+#                      |___________________________________|
+#
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+class CircularLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def add(self, data):
+        newNode = Node(data)
+        if self.head is None:
+            self.head = newNode
+            self.tail = newNode
+            newNode.next = None
+            newNode.prev = None
+        else:
+            self.tail.next = newNode
+            newNode.next = self.head
+            newNode.prev = self.tail
+            self.tail = newNode
+
+        self.length += 1
+
+    def __str__(self):
+        res = ""
+        temp = self.head
+        brk = False
+        for _ in range(self.length):
+            res += str(temp.data)
+            temp = temp.next
+            if temp.next is not self.head:
+                res += '->'
+        return  res
+
+Cll = CircularLinkedList()
+Cll.add(12)
+Cll.add(14)
+Cll.add(15)
+print(Cll.head.next.data)
--- LinkedList/DoublyLinkedList.py
+++ LinkedList/DoublyLinkedList.py
@@ -23,6 +23,7 @@ class DoublyLinkedList:
             newNode.prev = self.tail
             self.tail = newNode
             self.tail.next = None
+        self.length += 1
 
     def __str__(self):
         res = ''
@@ -30,15 +31,42 @@ class DoublyLinkedList:
         while temp is not None:
             res += str(temp.data)
             if temp.next is not None:
-                res += '<-->'
+                res += ' <--> '
             temp = temp.next
         return res
 
-dll = DoublyLinkedList()
-dll.addelememts(12)
-dll.addelememts(13)
-dll.addelememts(10)
+    def searchElements(self, value):
+        if value is None:
+            raise Exception("Value Cannot be Empty")
+        if self.length == 0:
+            return "DLL is Empty".capitalize()
+        temp = self.head
+        for _ in range(self.length):
+            if temp.data == value:
+                return True
+            if temp.next is None:
+                return False
+            temp = temp.next
 
+    def updateElementUsingValue(self, value, newValue):
+        if newValue == '' or newValue is None:
+            return "New value cannot be null or empty"
+        temp = self.head
+        for _ in range(self.length):
+            if temp.data == value:
+                temp.data = newValue
+                return "The Value {old} is updated to {new}".format(old = value, new= newValue)
+            if temp.next is None:
+                return str("Unable to find the value")
+            temp = temp.next
+
+dll = DoublyLinkedList()
+dll.addelememts(2)
+dll.addelememts(5)
+dll.addelememts(4)
+print("Dll before :", dll)
+print(dll.updateElementUsingValue(5, 3))
+print("Dll After :",dll)
 
 
 
